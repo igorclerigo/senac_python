e4ea3f272ac8c3ff64b05e50a01af584e92d1f2b
dokcreative <igorlima.clerigo@gmail.com>
2024-05-15 21:53:13 -0300

    Aula 07

---
+++ Programação front-end/Aula 07 - javascript/main.js
@@ -0,0 +1,50 @@
+var nome = prompt("Digite seu nome: ");
+alert("Olá " + nome);
+
+
+/* 
+
+alert("Olá");
+console.log("Olá");
+
+var x;
+x = 10;
+alert(x)
+
+var y;
+y = [1,10, "Olá", true];
+alert(y)
+
+
+if (my_var % 2 == 0){
+    alert("Número par");
+} 
+else if (my_var % 2 == 1){
+    alert("Número ímpar");
+}
+else{
+    alert("Valor inválido");
+}
+
+var i;
+for (i=0, i<4; i++){
+    alert(i)
+
+var i;
+i=0;
+while (i<4){
+    alert(i)
+    i++
+}
+
+}
+
+
+
+
+
+
+
+
+estudar operador ternário
+*/
