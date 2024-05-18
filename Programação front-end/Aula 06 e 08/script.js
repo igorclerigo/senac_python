b05d7b681f48a288732569ec6e0f6e7978d3dc05
dokcreative <igorlima.clerigo@gmail.com>
2024-05-17 21:58:37 -0300

    aula 98

---
+++ Programação front-end/Aula 06 e 08/script.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const storedUser = localStorage.getItem(username);
+    const userData = storedUser ? JSON.parse(storedUser) : {};
+
+    if (userData.password === password) {
+        alert('Login bem-sucedido!');
+        // Aqui, redirecione para a página principal do sistema ou mostre informações do usuário
+    } else {
+        alert('Usuário ou senha incorretos!');
+    }
+    window.location.href = 'index.html'
+});
